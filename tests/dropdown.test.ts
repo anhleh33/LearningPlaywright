a4368d5cfc27043afa1e6231e60365088bef2bdd
Anh Hoang intern TMA <anhlehoang2004@gmail.com>
2025-12-11 17:59:05 +0700

    add mulitple dropdown video 9

--- tests/dropdown.test.ts
+++ tests/dropdown.test.ts
@@ -7,14 +7,42 @@ test.describe('Handling dropdown', () => {
 
     test("Select dropdown based on the value", async ({ page }) => {
         const fruits = page.locator('#fruits')
+        await fruits.selectOption({ label: "Orange" })
         await fruits.selectOption("2")
         const msg = await page.locator('.notification.is-success')
         await expect(msg).toContainText("Orange");
     })
 
-    test('Select multiple dropdown of heroes', async({page}) => {
+    test('Select multiple dropdown of heroes', async ({ page }) => {
         const heroes = page.locator('#superheros')
-
         await expect(heroes).toBeVisible()
+        await heroes.selectOption([
+            { label: "Aquaman" },
+            { value: "bt" },
+            { index: 8 }
+        ])
+
+        const selectedValues = await heroes.evaluate((select) => {
+            const selectElement = select as HTMLSelectElement;
+            const options = Array.from(selectElement.selectedOptions);
+            return options.map(option => option.value);
+        });
+
+        console.log(selectedValues)
+
+        await expect(selectedValues.length).toBe(3)
+        await expect(selectedValues).toContain('aq');
+        await expect(selectedValues).toContain('bt');
+        await expect(selectedValues).toContain('dd');
+        const displayText = page.locator('.content.ng-star-inserted');
+        await expect(displayText).toContainText('Aquaman');
+    })
+
+    test('Count the selected value', async({page}) => {
+        const lang = await page.$$("#lang option")
+        console.log(lang.length)
+
+        // const lang = page.locator('#lang')
+        // console.log(lang.count())
     })
 })
