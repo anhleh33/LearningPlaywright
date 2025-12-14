9cb4ceada6f4e87a7259f452aecef2d6b15c7ad0
Anh Le Hoang <anhlehoang2004@gmail.com>
2025-12-14 11:27:07 +0700

    edit login test file

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -14,10 +14,9 @@ describe("Login Test", () => {
 
         await page.fill('input#id_login', 'koushik350@gmail.com');
         await page.fill('input#id_password', 'Pass123$');
+        // await page.locator('#tgnx8 > div > label > input[type=checkbox]').check()
         await page.click('signin_btn')
         //check toast msg
-
-        // await browser.close();
     });
 
     //npx playwright codegen https://leetcode.com/ 
@@ -25,7 +24,7 @@ describe("Login Test", () => {
         await page.goto('https://leetcode.com/');
         await page.getByRole('link', { name: 'Sign in' }).click();
         await page.locator('div').filter({ hasText: 'Sign InForgot Password?Sign' }).nth(5).click();
-        await page.locator('iframe[src="https://challenges.cloudflare.com/cdn-cgi/challenge-platform/h/g/turnstile/f/ov2/av0/rch/beguu/0x4AAAAAAAQrSHUTor4iGTpW/light/fbE/new/normal?lang=en"]').contentFrame().locator('body').click();
+        // await page.locator('iframe[src="https://challenges.cloudflare.com/cdn-cgi/challenge-platform/h/g/turnstile/f/ov2/av0/rch/beguu/0x4AAAAAAAQrSHUTor4iGTpW/light/fbE/new/normal?lang=en"]').contentFrame().locator('body').click();
         await page.getByRole('textbox', { name: 'Username or E-mail' }).click();
         await page.getByRole('textbox', { name: 'Username or E-mail' }).fill('helenjacqueline03us@gmail.com');
         await page.getByRole('textbox', { name: 'Password' }).click();
