5f170bef5d43abab0c04761dbb212c9f92f26ca6
Anh Le Hoang <anhlehoang2004@gmail.com>
2025-12-15 21:24:31 +0700

    add screenshot into playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // headless: false,
+    headless: false,
+    channel: "chrome",
     trace: 'on-first-retry',
+    screenshot: "only-on-failure"
   },
 
   /* Configure projects for major browsers */
