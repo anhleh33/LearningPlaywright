43887e1bc7d75181cf52e6791e4447ea5687ec71
Anh Le Hoang <anhlehoang2004@gmail.com>
2025-12-18 20:54:00 +0700

    bbb

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
+  // testMatch: ["drop.test.ts"],
   // reporter: [["dot"], ["json", { outputFile: "test-result.json" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
