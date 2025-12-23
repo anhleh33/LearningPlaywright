b19ba2f28e8df967d6d9f74b0fd5d5bb74f598d3
Anh Hoang intern TMA <anhlehoang2004@gmail.com>
2025-12-23 17:33:55 +0700

    test info parameter 35/100

--- tests/fixtures/myTest.test.ts
+++ tests/fixtures/myTest.test.ts
@@ -1,7 +1,10 @@
 import { fixture as test, expect }from './fixtures'
 
-test('Test sử dụng fixture tùy chỉnh', async ({ hey, page }) => {
+test('Test sử dụng fixture tùy chỉnh', async ({ hey, page }, info) => {
+    // info.skip()
     // Playwright sẽ tự động hiểu 'hey' là gì nhờ vào file .ts bạn đã viết
     console.log(hey); // In ra: "I am letcode"
     await expect(hey).toBe("I am letcode");
+
+    console.log("Is it passed? " + info.status)
 });
