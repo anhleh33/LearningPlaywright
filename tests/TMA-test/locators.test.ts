6c30281b3859c737ad1bcd563f2757d145dee293
Anh Hoang intern TMA <anhlehoang2004@gmail.com>
2026-01-13 15:40:22 +0700

    locator

---
+++ tests/TMA-test/locators.test.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { waitForDebugger } from 'inspector';
+
+test('Locators', async ({ page }) => {
+    await page.goto("https://letcode.in/dropdowns")
+    
+    const dropdown = await page.getByRole('combobox', { name: 'Select Fruit' });
+    await dropdown.selectOption({ label: 'Orange' });
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://dkq832w8boiiy.cloudfront.net/#/login');
+  await page.getByRole('textbox', { name: 'Enter Username' }).fill('testuser@gmail.com');
+  await page.getByRole('textbox', { name: 'Enter Password' }).fill('12345678x@X');
+  await page.getByRole('button', { name: 'Login', exact: true }).click();
+});
+
+test('test', async ({ page }) => {
+    await page.goto('https://letcode.in/dropdowns');
+    await page.locator('#fruits').selectOption('2');
+    await page.locator('#superheros').selectOption('aq');
+    await page.locator('#lang').selectOption('swift');
+    await page.getByRole('link', { name: 'Work-Space' }).click();
+});
